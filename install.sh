fc9233f4029fbb3d8eaeba3205368cea1df9e935
Miguel Reis <miguel_reis08@protonmail.com>
2023-02-28 23:12:32 -0300

    Update script

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ lsblk
 printf "\nChoose the disk for the installation: "
 read mydisk
 mydisk=/dev/\${mydisk}
-mypartition=\${mydisk}1
+mypartition=\${mydisk}/1
 
-printf "\nGive a name to the encrypted partition: "
+printf "Give a name to the encrypted partition: "
 read lukspartition
 
 # Pre-chroot system configuration
@@ -45,8 +45,10 @@ xchroot /mnt <<- CHROOT
   # GRUB configuration
   echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 
-  # Getting the UUID of the system mypartitionition
+  # Getting the UUID of the system partition
   myuuid=$(blkid -o value -s UUID \$mypartition)
+  echo "GRUB_CMDLINE_LINUX_DEFAULT=rd.luks.uuid=${myuuid}" >> /etc/default/grub
+  # sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=/GRUB_CMDLINE_LINUX_DEFAULT=rd.luks.uuid=${myuuid}/g" /etc/default/grub
 
   # LUKS key setup
   dd bs=1 count=64 if=/dev/urandom of=/boot/volume.key
