7f855cf4b2a64e7049ce0980d7c3f3e1222f54cd
Miguel Reis <miguel_reis08@protonmail.com>
2023-03-01 11:06:56 -0300

    Update script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# RUN THIS SCRIPT AS ROOT
+
 # Disk and partition variables
 lsblk
 printf "\nChoose destination disk for the installation: "
@@ -14,27 +16,25 @@ read -r lukspartition
 scriptdir=$(pwd)
 
 # Pre-chroot system configuration
-cat <<- EOF | sudo su
-  # Format destination disk
-  echo 'type=83' | sfdisk "${mydisk}"
-  
-  # Configure encrypted partition
-  cryptsetup luksFormat --type luks1 "${mypartition}"
-  cryptsetup luksOpen \$mypartition "${lukspartition}"
-  mkfs.btrfs /dev/mapper/"${lukspartition}"
-  
-  # System installation
-  mount /dev/mapper/"${lukspartition}" /mnt
-  mkdir -p /mnt/var/db/xbps/keys
-  cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
-  xbps-install -Sy -R https://repo-default.voidlinux.org/current/musl -r /mnt base-system cryptsetup grub
-  
-  # Copying grub configuration file
-  cp "${scriptdir}"/grub /mnt/etc/default/grub
-EOF
+# Format destination disk
+echo 'type=83' | sfdisk "$mydisk"
+
+# Configure encrypted partition
+cryptsetup luksFormat --type luks1 "$mypartition"
+cryptsetup luksOpen "$mypartition" "$lukspartition"
+mkfs.btrfs /dev/mapper/"$lukspartition"
+
+# System installation
+mount /dev/mapper/"$lukspartition" /mnt
+mkdir -p /mnt/var/db/xbps/keys
+cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
+xbps-install -Sy -R https://repo-default.voidlinux.org/current/musl -r /mnt base-system cryptsetup grub
+
+# Copying grub configuration file
+cp "${scriptdir}"/grub /mnt/etc/default/grub
 
 # Entering chroot
-cat <<- CHROOT | sudo xchroot /mnt 
+cat <<- CHROOT | xchroot /mnt 
 
   # Initial configuration
   chown root:root /
@@ -50,6 +50,7 @@ cat <<- CHROOT | sudo xchroot /mnt
 
   # GRUB configuration
   sed -i "s|<UUID>|${myuuid}|g" /etc/default/grub
+  
 
   # LUKS key setup
   dd bs=1 count=64 if=/dev/urandom of=/boot/volume.key
